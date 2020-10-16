f641cef7241406a2985792db93bdf502ba68926d
Kofi Gumbs <h.kofigumbs@gmail.com>
2020-10-15 20:44:23 -0400

    Always enable developer tools (#41)

--- Sources/Shared/WKWebView+enableDevelop.swift
+++ Sources/Shared/WKWebView+enableDevelop.swift
@@ -2,8 +2,6 @@ import WebKit
 
 extension WKWebView {
     public func enableDevelop() {
-        #if DEBUG
         self.configuration.preferences.setValue(true, forKey: "developerExtrasEnabled")
-        #endif
     }
 }
