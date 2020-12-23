7787118d600eac1303a01ea58272040290ed9783
Kofi Gumbs <h.kofigumbs@gmail.com>
2020-12-23 12:30:43 -0500

    Revert "Remove macOS version from user agent"
    This reverts commit 737ec4f306f820389871a9efdf5c0403fee4da22.

--- Sources/Runtime/Browser.swift
+++ Sources/Runtime/Browser.swift
@@ -5,7 +5,7 @@ class Browser: NSObject {
     static let global = Browser()
 
     // Fake a more popular browser to circumvent UA-sniffing
-    static let userAgent = "Mozilla/5.0 AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15"
+    static let userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15"
 
     static let blocklist: String = {
         guard let url = Bundle.multi?.url(forResource: "blocklist", withExtension: "json"),
