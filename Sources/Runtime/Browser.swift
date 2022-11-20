e69b823c3c4cbde51d819c897db814345d3942fd
Kofi Gumbs <h.kofigumbs@gmail.com>
2022-11-19 21:01:05 -0500

    Inline Browser extensions

--- Sources/Runtime/Browser.swift
+++ Sources/Runtime/Browser.swift
@@ -40,4 +40,30 @@ class Browser: NSObject {
             NSWorkspace.shared.open(url, configuration: configuration)
         }
     }
+
+    func customCss(_ configuration: WKWebViewConfiguration, urls: [URL]) {
+        guard !urls.isEmpty else { return }
+        let css = urls
+            .compactMap { try? String(contentsOf: $0) }
+            .joined()
+            .data(using: .utf8)?
+            .base64EncodedString()
+        let js = """
+            const style = document.createElement("style");
+            style.innerText = atob("\(css ?? "")");
+            document.querySelector("head").insertAdjacentElement("beforeend", style);
+        """
+        configuration.userContentController.addUserScript(
+            WKUserScript(source: js, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
+        )
+    }
+
+    func customJs(_ configuration: WKWebViewConfiguration, urls: [URL]) {
+        guard !urls.isEmpty else { return }
+        urls.compactMap { try? String(contentsOf: $0) }.forEach { js in
+            configuration.userContentController.addUserScript(
+                WKUserScript(source: js, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
+            )
+        }
+    }
 }
