30e91b7fae81aa6c2d8a5f7e6e243a537eab2781
Kofi Gumbs <h.kofigumbs@gmail.com>
2022-11-19 23:46:49 -0500

    Run create-mac-app without tmp script, fixes #85

--- Sources/Shared/Form.swift
+++ Sources/Shared/Form.swift
@@ -15,23 +15,38 @@ class Form: NSObject, WKScriptMessageHandler {
             fail("Cannot load your configuration.")
             return
         }
-        guard let createMacApp = Bundle.multi?.url(forResource: "create-mac-app", withExtension: nil),
-              Script.run(createMacApp, environment: [
-                  "APP_NAME": name,
-                  "ICON_PATH": icon.selected?.path ?? "",
-                  "JSON_CONFIG": json,
-                  "OVERWRITE": overwrite ? "1" : "0",
-                  "RELAUNCH_PID": "\(ProcessInfo.processInfo.processIdentifier)",
-                  "UI": "1",
-              ]) else {
-            fail("Cannot allocate configuration script.")
+        guard let createMacApp = Bundle.multi?.url(forResource: "create-mac-app", withExtension: nil) else {
+            fail("Cannot find create-mac-app.")
             return
         }
+
+        let process = Process()
+        process.environment = [
+            "MULTI_APP_NAME": name,
+            "MULTI_ICON_PATH": icon.selected?.path ?? "",
+            "MULTI_JSON_CONFIG": json,
+            "MULTI_OVERWRITE": overwrite ? "1" : "0",
+            "MULTI_RELAUNCH_PID": "\(ProcessInfo.processInfo.processIdentifier)",
+            "MULTI_UI": "1",
+        ]
+        process.executableURL = createMacApp
+
+        let pipe = Pipe()
+        process.standardError = pipe
+        process.standardOutput = pipe
+        process.terminationHandler = { process in
+            if process.terminationStatus != 0 {
+                let output = String(data: pipe.fileHandleForReading.availableData, encoding: .utf8)
+                self.fail(output ?? "create-mac-app exited with code \(process.terminationStatus)")
+            }
+        }
+
+        try! process.run()
     }
 
     private func fail(_ reason: String) {
         let alert = NSAlert()
-        alert.messageText = "Multi was unable to create your app"
+        alert.messageText = "Multi run into an issue creating your app"
         alert.informativeText = reason
         alert.alertStyle = .critical
         alert.runModal()
